256c40baa2bd0008dffd308f74be42177fc9c218
Liam Sharpe <liamsmail1234@gmail.com>
2019-04-11 15:04:06 +1200

    Followed tutorial to make a location app

--- lib/main.dart
+++ lib/main.dart
@@ -1,177 +1,252 @@
+//import 'package:flutter/material.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:sms/sms.dart';
+//
+//void main() => runApp(MyApp());
+//
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Emergency Help App',
+//      theme: ThemeData(
+//        primarySwatch: Colors.purple,
+//      ),
+//      home: MyHomePage(),
+//    );
+//  }
+//}
+//
+//class _Menu extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Drawer(
+//      child: Column(
+//        children: <Widget>[
+//          Expanded(
+//            child: ListView(
+//              children: <Widget>[
+//                DrawerHeader(
+//                  // todo: Change this to the app logo when its been created
+//                  child: Text("App logo will be here eventually\n"
+//                      "For now, enjoy this poem extract by E.A. Poe:\n\n"
+//                      "Years of love have been forgot,\n"
+//                      "in the hatred of a minute"),
+//                ),
+//                ListTile(
+//                  leading: Icon(Icons.home),
+//                  title: Text('Home'),
+//                  onTap: () {},
+//                ),
+//                ListTile(
+//                  leading: Icon(Icons.contacts),
+//                  title: Text('Contacts'),
+//                  onTap: () {},
+//                ),
+//                ListTile(
+//                  leading: Icon(Icons.map),
+//                  title: Text('Map / List'),
+//                  onTap: () {},
+//                ),
+//                ListTile(
+//                  leading: Icon(Icons.chat),
+//                  title: Text('Chat'),
+//                  onTap: () {},
+//                ),
+//              ],
+//            ),
+//          ),
+//          Container(
+//              child: Align(
+//                  alignment: FractionalOffset.bottomCenter,
+//                  child: Container(
+//                      child: Column(
+//                    children: <Widget>[
+//                      Divider(),
+//                      ListTile(
+//                        leading: Icon(Icons.settings),
+//                        title: Text('Settings'),
+//                        onTap: () {},
+//                      ),
+//                      ListTile(
+//                        leading: Icon(Icons.info),
+//                        title: Text('About'),
+//                        onTap: () {},
+//                      )
+//                    ],
+//                  ))))
+//        ],
+//      ),
+//    );
+//  }
+//}
+//
+//class MyHomePage extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text("Emergency Help App"),
+//      ),
+//      drawer: _Menu(),
+//      body: Container(
+//        height: double.maxFinite,
+//        decoration: BoxDecoration(
+//          gradient: LinearGradient(
+//            begin: Alignment.topRight,
+//            end: Alignment.bottomLeft,
+//            colors: [
+
+////              Theme.of(context).backgroundColor,
+//              Colors.purple[300],
+//              Theme.of(context).primaryColorLight
+//            ],
+//          ),
+//        ),
+//        child: Stack(
+//          children: <Widget>[
+//            Column(
+//              children: <Widget>[
+//                Padding(
+//                  padding: const EdgeInsets.symmetric(
+//                      vertical: 50.0,
+//                      horizontal: 20.0
+//                  ),
+//                  child: Center(
+//                    child: Text(
+//                      "Send location by clicking help",
+//                      style: TextStyle(
+//                        color: Colors.white,
+//                        fontSize: 20.0,
+//                      ),
+//                    ),
+//                  ),
+//                ),
+//              ],
+//            ),
+//            Padding(
+//              padding: const EdgeInsets.all(50.0),
+//              child: OutlineButton(
+//                shape: CircleBorder(),
+//                color: Theme.of(context).backgroundColor,
+//                onPressed: () {
+//                  Firestore.instance.collection('message').document()
+//                      .setData({ 'date': new DateTime.now()});
+//
+//                  SmsSender sender = new SmsSender();
+//                  String address = "0277764722";                              // todo Replace with mobile number
+//                  sender.sendSms(new SmsMessage(address, 'Hello flutter!'));
+//                },
+//                child: Center(
+//                  child: Text(
+//                    "Help",
+//                    style: TextStyle(
+//                      color: Colors.white,
+//                      fontSize: 48.0,
+//                    ),
+//                  ),
+//                ),
+//              ),
+//            ),
+//            Padding(
+//              padding: const EdgeInsets.all(20.0),
+//              child: Align(
+//                alignment: FractionalOffset.bottomCenter,
+//                child: ButtonTheme(
+//                  minWidth: double.infinity,
+//                  shape: RoundedRectangleBorder(
+//                      borderRadius: BorderRadius.circular(10.0)),
+//                  height: 50.0,
+//                  child: OutlineButton(
+//                    color: Theme.of(context).backgroundColor,
+//                    child: Text(
+//                      "More Help...",
+//                      style: TextStyle(
+//                        color: Colors.white,
+//                        fontSize: 18.0,
+//                      ),
+//                    ),
+//                    onPressed: () {},
+//                  ),
+//                ),
+//              ),
+//            )
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
+
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:sms/sms.dart';
+import 'package:location/location.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Emergency Help App',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: MyHomePage(),
-    );
-  }
+  State<StatefulWidget> createState() => MyAppState();
 }
 
-class _Menu extends StatelessWidget {
+class MyAppState extends State<MyApp> {
+  Map<String, double> currentLocation = new Map();
+  StreamSubscription<Map<String, double>> locationSubscription;
+
+  Location location = new Location();
+
+  String error;
+
+  @override
+  void initState() {
+    super.initState();
+
+    currentLocation['latitude'] = 0.0;
+    currentLocation['longitude'] = 0.0;
+
+    initPlatformState();
+    locationSubscription =
+        location.onLocationChanged().listen((Map<String, double> result) {
+      setState(() {
+        currentLocation = result;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView(
-              children: <Widget>[
-                DrawerHeader(
-                  // todo: Change this to the app logo when its been created
-                  child: Text("App logo will be here eventually\n"
-                      "For now, enjoy this poem extract by E.A. Poe:\n\n"
-                      "Years of love have been forgot,\n"
-                      "in the hatred of a minute"),
-                ),
-                ListTile(
-                  leading: Icon(Icons.home),
-                  title: Text('Home'),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.contacts),
-                  title: Text('Contacts'),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.map),
-                  title: Text('Map / List'),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.chat),
-                  title: Text('Chat'),
-                  onTap: () {},
-                ),
-              ],
+    return new MaterialApp(
+        home: new Scaffold(
+            appBar: AppBar(
+              title: Text("Location"),
             ),
-          ),
-          Container(
-              child: Align(
-                  alignment: FractionalOffset.bottomCenter,
-                  child: Container(
-                      child: Column(
+            body: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Divider(),
-                      ListTile(
-                        leading: Icon(Icons.settings),
-                        title: Text('Settings'),
-                        onTap: () {},
-                      ),
-                      ListTile(
-                        leading: Icon(Icons.info),
-                        title: Text('About'),
-                        onTap: () {},
-                      )
-                    ],
-                  ))))
-        ],
-      ),
-    );
+                  Text(
+                      "Lat/Lng${currentLocation['latitude']}/${currentLocation['longitude']}",
+                      style:
+                          TextStyle(fontSize: 20.0, color: Colors.blueAccent))
+                ]))));
   }
-}
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Emergency Help App"),
-      ),
-      drawer: _Menu(),
-      body: Container(
-        height: double.maxFinite,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-//              Theme.of(context).backgroundColor,
-              Colors.purple[300],
-              Theme.of(context).primaryColorLight
-            ],
-          ),
-        ),
-        child: Stack(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 50.0,
-                      horizontal: 20.0
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Send location by clicking help",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: OutlineButton(
-                shape: CircleBorder(),
-                color: Theme.of(context).backgroundColor,
-                onPressed: () {
-                  Firestore.instance.collection('message').document()
-                      .setData({ 'date': new DateTime.now()});
-                  SmsSender sender = new SmsSender();
-                  String address = "MOBILE NO HERE";                              // todo Replace with mobile number
-                  sender.sendSms(new SmsMessage(address, 'Hello flutter!'));
-                },
-                child: Center(
-                  child: Text(
-                    "Help",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 48.0,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Align(
-                alignment: FractionalOffset.bottomCenter,
-                child: ButtonTheme(
-                  minWidth: double.infinity,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  height: 50.0,
-                  child: OutlineButton(
-                    color: Theme.of(context).backgroundColor,
-                    child: Text(
-                      "More Help...",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                      ),
-                    ),
-                    onPressed: () {},
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+  void initPlatformState() async {
+    Map<String, double> my_location;
+    try {
+      my_location = await location.getLocation();
+      error = "";
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED')
+        error = 'Permission denied';
+      else if (e.code == "PERMISSION_DENIED_NEVER_ASK")
+        error =
+            "Permission denied - please ask the user to enable it from the app settings";
+      my_location = null;
+    }
+    setState(() {
+      currentLocation = my_location;
+    });
   }
 }
